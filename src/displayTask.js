3e8e7fc9de1dbdead4da9e3ea748d8f47f3d1fa5
Louis Doan <louisdoan9@gmail.com>
2022-05-13 21:25:21 -0400

    Add way to remove tasks

--- src/displayTask.js
+++ src/displayTask.js
@@ -2,23 +2,30 @@ import { Task } from './createTasks';
 
 function displayTask(Task) {
 	let task = document.createElement('div');
-	document.querySelector('#content').appendChild(task);
 	task.className = 'task';
+	document.querySelector('#content').appendChild(task);
 
 	let name = document.createElement('div');
 	let description = document.createElement('div');
 	let dueDate = document.createElement('div');
 	let priority = document.createElement('div');
 
-	name.innerText = Task.name;
-	description.innerText = Task.description;
-	dueDate.innerText = Task.dueDate;
-	priority.innerText = Task.priority;
+	name.innerText = `Name: ${Task.name}`;
+	description.innerText = `Description: ${Task.description}`;
+	dueDate.innerText = `Due Date: ${Task.dueDate}`;
+	priority.innerText = `Priority: ${Task.priority}`;
 
 	task.appendChild(name);
 	task.appendChild(description);
 	task.appendChild(dueDate);
 	task.appendChild(priority);
+
+	let deleteButton = document.createElement('button');
+	deleteButton.innerText = 'X';
+	deleteButton.addEventListener('click', () => {
+		deleteButton.parentNode.parentNode.removeChild(deleteButton.parentNode);
+	});
+	task.appendChild(deleteButton);
 }
 
 export { displayTask };
