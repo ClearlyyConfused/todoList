548cc2e7218432e104d456d370b002a52a006c6d
Louis Doan <louisdoan9@gmail.com>
2022-05-17 22:02:13 -0400

    Add styles and labels to task form

--- src/createForm.js
+++ src/createForm.js
@@ -8,20 +8,28 @@ function createFormInputs() {
 		let inputField = document.createElement('input');
 		inputField.setAttribute('input', 'text');
 		inputField.setAttribute('id', x);
-		inputFieldArr.push(inputField);
+
+		let label = document.createElement('label');
+		label.setAttribute('for', x);
+		label.innerText = `${x.charAt(0).toUpperCase() + x.slice(1)}:`;
+
+		let input = document.createElement('div');
+		input.appendChild(label);
+		input.appendChild(inputField);
+		inputFieldArr.push(input);
 	}
 	return inputFieldArr;
 }
 
 function createExitButton() {
-	let exitButton = document.createElement('div');
+	let exitButton = document.createElement('button');
 	exitButton.innerText = 'X';
 	exitButton.addEventListener('click', hideForm);
 	return exitButton;
 }
 
 function createEnterButton(project) {
-	let enterButton = document.createElement('div');
+	let enterButton = document.createElement('button');
 	enterButton.innerText = 'Enter';
 	enterButton.addEventListener('click', () => {
 		let newTask = getFormData();
